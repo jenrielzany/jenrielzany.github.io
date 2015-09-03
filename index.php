0907fdb5f3bb460f20fbec2bbf0a00278e72367f
Jenriel <jenrielzany@gmail.com>
2015-09-04 03:19:43 +0800

    Updates

---
+++ index.php
@@ -0,0 +1,321 @@
+<!DOCTYPE html>
+<!--[if lt IE 7]> <html class="no-js lt-ie9 lt-ie8 lt-ie7" lang="en"> <![endif]-->
+<!--[if IE 7]>    <html class="no-js lt-ie9 lt-ie8" lang="en"> <![endif]-->
+<!--[if IE 8]>    <html class="no-js lt-ie9" lang="en"> <![endif]-->
+<!-- Consider adding a manifest.appcache: h5bp.com/d/Offline -->
+<!--[if gt IE 8]><!--> 
+<html class="no-js" lang="en"> <!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+	<title> Zany Graphics &amp; Designs</title>
+	<meta name="description" content="">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.4.0/css/font-awesome.min.css">
+
+	<link rel="stylesheet" href="min-css/layout.min.css">
+</head>
+<body>
+<div class="main-header wow fadeIn">
+	<div class="main-mnu">
+		<div class="w960 grid">
+			<ul class="mnu">
+				<li> Home </li>
+				<li> Works </li>
+				<li> About Me </li>
+				<li> Contact </li>
+			</ul>
+		</div>
+
+		<div class="row">
+			<div class="header-con">
+				<div class="w960 grid">
+					<div class="c1"></div>
+						<div class="c10">
+							<div class="logo-con wow fadeInDown">
+								<img src="img/logo.png" alt="logo">
+							</div>
+							<h1 class="hdr-ttle wow fadeInDown">BRILLIANT DESIGNER. BRILLIANT PRODUCTS</h1>
+							<p class="hdr-par wow fadeIn" data-wow-delay=".5s">
+								I deliver awesome designs that meets your desires. Make brilliant ideas that are out of the box. Always reliable and passionate to the projects I am working with. Wink!
+							</p>
+
+							<p class="cntr wow fadeInUp"> <button class="hdr-btn"> LETS GET STARTED </button></p>
+						</div>
+					<div class="c1"></div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+
+<!-- ======= Slider Menu on Mobile ========= -->
+		<nav id="slide-menu">
+			  <ul>
+			  	<h1 class="orange-title wow fadeInLeft"><i class="fa fa-tasks"></i>MAIN MENU</h1>
+				  <li class="wow fadeInLeft" data-wow-delay=".3s"> <a href="index.php"><i class="fa fa-home"></i>Home</a> </li>
+					<li class="wow fadeInLeft" data-wow-delay=".5s"> <a href="how-it-works.php"><i class="fa fa-rocket"></i> Works</a> </li>
+					<li class="wow fadeInLeft" data-wow-delay=".7s"> <a href="why-go-solar.php"><i class="fa fa-soccer-ball-o"></i>About Me</a> </li>
+					<li class="wow fadeInLeft" data-wow-delay="1s"> <a href="save-money.php"><i class="fa fa-phone-square"></i>Contact</a> </li>
+			  </ul>
+			</nav>
+			<!-- Content panel -->
+			<div id="content">
+			  <div class="menu-trigger">
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			    <span class="icon-bar"></span>
+			  </div>
+			</div>
+		<!-- ======= End of Slider Menu on Mobile ========= -->
+
+		<div class="how">
+			<div class="w960 grid">
+
+				<h1 class="section-hdrs clear wow fadeIn"> HOW I DO IT </h1>
+				<div class="c6">
+
+					<div class="row clear wow fadeInUp">
+						<div class="c4 clear">
+							<div class="img-con">
+								<img src="img/donut-graph.png" alt="">
+							</div>
+						</div>
+
+						<div class="c8 clear wow fadeInUp" data-wow-delay=".3s">
+							<div class="desc-con">
+								<h5 class="hdr-how"> HIGH QUALITY</h5>
+								<p class="how-par">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus iure maiores, sapiente at dolores! Porro ipsam quaerat.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+
+				<div class="c6">
+					<div class="row clear wow fadeInUp">
+						<div class="c4 clear">
+							<div class="img-con">
+								<img src="img/clock.png" alt="">
+							</div>
+						</div>
+
+						<div class="c8 clear wow fadeInUp" data-wow-delay=".3s">
+							<div class="desc-con">
+								<h5 class="hdr-how"> RIGHT ON TIME </h5>
+								<p class="how-par">
+									Lorem ipsum dolor sit amet, consectetur adipisicing elit. Delectus iure maiores, sapiente at dolores! Porro ipsam quaerat.
+								</p>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+
+		<div class="sharped-bg">
+			<div class="w960 grid">
+				<div class="row">
+					<h1 class="section-hdrs wow fadeInUp"> MY WORKS </h1>
+					<div class="works-con">
+						<div class="c6 wow fadeInUp">
+							<h1 class="works-hdr"> GREENWIRE UNIFIED CUSTOMER MANAGEMENT APP.</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<a href="https://www.behance.net/gallery/29179505/Greenwire-Outsourcing-Inc-Internal-Dashboard" target="_blank">
+								<button class="works-btn"> VIEW PROJECT </button>
+							</a>
+						</div>
+
+						<div class="c6 wow fadeInUp" data-wow-delay=".3s">
+							<img src="img/gw-project.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="works-con">
+						<div class="c6 wow fadeInUp">
+							<h1 class="works-hdr"> GREENWIRE UNIFIED CUSTOMER MANAGEMENT APP.</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<a href="http://solarsavingsusa.com" target="_blank">
+								<button class="works-btn"> VIEW PROJECT </button>
+							</a>
+						</div>
+
+						<div class="c6 wow fadeInUp" data-wow-delay=".3s">
+							<img src="img/solar-power.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="works-con">
+						<div class="c6 wow fadeInUp">
+							<h1 class="works-hdr"> GREENWIRE MONGGOS</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<a href="https://www.behance.net/gallery/29022615/Zany-Logo-Revision-20" target="_blank">
+								<button class="works-btn"> VIEW PROJECT </button>
+							</a>
+						</div>
+
+						<div class="c6 wow fadeInUp" data-wow-delay=".3s">
+							<img src="img/zny.png" alt="">
+						</div>
+					</div>
+				</div>
+
+				<div class="row">
+					<div class="works-con">
+						<div class="c6 wow fadeInUp">
+							<h1 class="works-hdr"> GREENWIRE MONGGOS</h1>
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<p>
+								Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aut beatae non in dolor blanditiis harum facilis, repellendus sequi, autem debitis inventore doloribus iusto libero vero ut tenetur, ducimus iure iste?
+							</p>
+
+							<a href="https://www.behance.net/gallery/16394475/Xcom" target="_blank">
+								<button class="works-btn"> VIEW PROJECT </button>
+							</a>
+						</div>
+
+						<div class="c6 wow fadeInUp" data-wow-delay=".3s">
+							<img src="img/xcom.png" alt="">
+						</div>
+					</div>
+				</div>
+				
+				<div class="row">
+					<div class="c3"></div>
+						<div class="c6">
+							<p class="cntr"> 
+								<a href="http://behance.net/zanygd" target="_blank">
+									<button class="view-more"> VIEW MORE PROJECTS </button>
+								</p>
+							</a>
+						</div>
+					<div class="c3"></div>
+				</div>
+			</div>
+		</div>
+
+	
+
+	<div class="about-section">
+		<div class="w960 grid">
+			<h3 class="abt-hdr wow fadeIn"> ABOUT ME </h3>
+			
+			<div class="c2"></div>
+
+			<div class="c8">
+				<p class="cntr wow fadeIn">
+					<img src="img/about-img.png" alt="">
+				</p>
+
+				<h5 class="nme wow fadeIn"> JENRIEL VICTOR M. JADRAQUE</h5>
+				<p class="job wow fadeIn"> Junior Web Designer </p>
+
+				<p class="abt-par cntr wow fadeIn">
+					vI have worked for a long time with designing. I have not read a lot but I have done many designs not knowing the concepts and principles of some elements of designing. It took me a while to decide to read these stuffs and learned a lot the principles of designing and techniques. 
+				</p>
+
+				<p class="abt-par cntr wow fadeIn" data-wow-delay=".3s">
+					vI have worked for a long time with designing. I have not read a lot but I have done many designs not knowing the concepts and principles of some elements of designing. It took me a while to decide to read these stuffs and learned a lot the principles of designing and techniques. 
+				</p>
+			</div>
+
+			<div class="c2"></div>
+		</div>
+		
+		<div class="w640 grid">
+			<div class="c4">
+				<p class="cntr wow fadeInDown">
+					<i class="fa fa-heart abt-lhd"></i>
+				</p>
+
+				<h4 class="cat wow fadeInDown"> LIKES </h4>
+
+				<ul class="lhd wow fadeIn">
+					<li>Perfection</li>
+					<li>Greens</li>
+					<li>Food</li>
+					<li>Music</li>
+					<li>Arts</li>
+				</ul>
+			</div>
+
+			<div class="c4">
+				<p class="cntr wow fadeInDown" data-wow-delay=".3s">
+					<i class="fa fa-camera abt-lhd"></i>
+				</p>
+
+				<h4 class="cat wow fadeInDown" data-wow-delay=".3s"> HOBBY </h4>
+				<ul class="lhd wow fadeIn">
+					<li>Playing Guitar</li>
+					<li>Listening to Goood Music</li>
+					<li>Sketching </li>
+					<li>Playing Video Games</li>
+					<li>Playing with Dog</li>
+				</ul>
+			</div>
+
+			<div class="c4">
+				<p class="cntr wow fadeInDown" data-wow-delay=".5s">
+					<i class="fa fa-times-circle-o abt-lhd"></i>
+				</p>
+
+				<h4 class="cat wow fadeInDown" data-wow-delay=".5s"> DISLIKES </h4>
+				<ul class="lhd wow fadeIn">
+					<li>Annoying People</li>
+					<li>Scratching Sound</li>
+					<li>Beer</li>
+					<li>Dirt</li>
+				</ul>
+			</div>
+		</div>
+	</div>
+
+	<div class="ftr">
+		<div class="w960 grid">
+			<div class="c7 clear">
+				<p class="ftr-par wow fadeIn"> Copyright &copy; 2015. Zany Graphics &amp; Designs. All Rights Reserved.</p>
+			</div>
+			<div class="c3 clear"><p class="ftr-cntc wow fadeIn"><i class="fa fa-paper-plane"></i> jenrielzany@gmail.com </p></div>
+			<div class="c2 clear"><p class="ftr-cntc wow fadeIn"><i class="fa fa-behance"></i> behance.net/zanygd </p></div>
+		</div>
+	</div>
+	
+	<!-- ================ Scripts ================ -->
+	<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+	<script src="wow/dist/wow.js"></script>
+	<script src="js/slide.js"></script>
+	<script src="wow/dist/wow.min.js"></script>
+	<script>
+	 new WOW().init();
+	</script>
+</body>
+</html>
